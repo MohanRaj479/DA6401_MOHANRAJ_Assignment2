47513819441ea002870f445e8ac0b6024d722f0a
MOHANRAJ M <150114336+MohanRaj479@users.noreply.github.com>
2026-04-14 18:21:01 +0530

    Update multitask.py

--- models/multitask.py
+++ models/multitask.py
@@ -23,7 +23,7 @@ class MultiTaskPerceptionModel(nn.Module):
         import gdown
         gdown.download(id="1qBwaQpxDPeNO5IQjFlE_RAxsij9Un-dS", output=classifier_path, quiet=False)
         gdown.download(id="1RN-aI-Yb6Hbn8k6QRC7-65WOEiTj3YZk", output=localizer_path, quiet=False)
-        gdown.download(id="1Xend2wMSTsnZKqMp-Vber1plTSdbhUFp", output=unet_path, quiet=False)
+        gdown.download(id="18NE3qZ34xyagPQYqzD9zjQE2c5Bddu_p", output=unet_path, quiet=False)
         
         self.shared_encoder = VGG11Encoder(in_channels)
         dummy_classifier = VGG11Classifier(num_breeds, in_channels)
